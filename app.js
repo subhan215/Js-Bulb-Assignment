ed0b09580ae59ef37a94637360462adc45e9320a
subhan215 <63653805+subhan215@users.noreply.github.com>
2021-11-20 11:34:43 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+/* function textcolor() {
+    var TextColor = document.getElementsByTagName("h1");
+
+    // TextColor[0].style.color = prompt("Enter text color for first heading");
+    // TextColor[1].style.color = prompt("Enter text color for second heading"); 
+    
+     for(var i = 0; i < TextColor.length; i++){
+        TextColor[i].style.color = prompt("Enter color for heading " + (i + 1))
+    }
+} 
+function bgcolor() {
+    var bgColor = document.getElementsByTagName("h1")
+   //  bgColor[0].style.background = prompt("Enter background color for heading 1 ");
+
+   // bgColor[1].style.background = prompt("Enter background color for heading 2 "); 
+    for(var i = 0; i < bgColor.length; i++){
+        bgColor[i].style.background = prompt("Enter color for heading " + (i + 1))
+    }
+
+} */
+/* function big() {
+    var image = document.getElementsByTagName("img");
+   image[0].className = "bigimg";
+}
+function small() {
+    var image = document.getElementsByTagName("img");
+   image[0].className = "smallimg";
+}
+function hide() {
+    var image = document.getElementsByTagName("img");
+   image[0].className = "hideimg";
+}
+function show(){
+    var image = document.getElementsByTagName("img");
+    image[0].className = "showimg";
+} */
+/* function bulbtoggler(value) {
+    var bulbToggler = document.getElementById("img");
+    if(value == 1){
+        bulbToggler.src = "Images/bulb on.jpg";
+    } else if(value == 0){
+        bulbToggler.src = "Images/bulb off.jpg";
+    }
+
+
+} 
+function bulbon(keyword){
+         keyword.src = "Images/bulb on.jpg"
+}
+function bulboff(keyword){
+    keyword.src = "Images/bulb off.jpg"
+} */
+/* function paracolor(){
+    var para = document.getElementsByTagName("p");
+    para[0].style.color = prompt("Enter color for para 1")
+    para[1].style.color = prompt("Enter color for para 2")
+    para[2].style.color = prompt("Enter color for para 3")
+    para[3].style.color = prompt("Enter color for para 4")
+} */
